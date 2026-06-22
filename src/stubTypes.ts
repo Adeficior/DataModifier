f40fc012bc49a1d3f3cca48cce822bda8177a9b0
Niklas Widmann <nik@somethingcatchy.net>
2026-06-22 13:20:43 +0200

    reference stub types using tsconfig paths

---
+++ src/stubTypes.ts
@@ -0,0 +1,14 @@
+export type RegistryId = string;
+
+export type CreativeModeTabId = string;
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export type InferIds<T extends RegistryId> = string;
+
+export type ItemId = string;
+
+export type BlockId = string;
+
+export type FluidId = string;
+
+export type RecipeSerializerId = string;
