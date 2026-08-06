62d919eeee62766127ff117e5b7fb2a937dbcdad
Niklas Widmann <nik@somethingcatchy.net>
2026-08-06 22:46:21 +0200

    fix remaining syntax errors

--- modules/lang/src/module.ts
+++ modules/lang/src/module.ts
@@ -1,13 +1,14 @@
 import { defineModule } from "@adeficior/data-modifier-core";
+import { name } from "../package.json";
 import { LangEmitter } from "./emitter";
 import { LangLoader } from "./loader";
 
 export default defineModule({
-  setupLoaders: ({ register }) => {
-    register("lang", new LangLoader());
-  },
-  setupEmitters: ({ register, get }) => {
-    const loader = get("loader:lang");
-    register("lang", new LangEmitter(loader));
+  importModule: name,
+  setup: (pack) => {
+    const loader = pack.loader("lang", () => new LangLoader());
+    pack.emitter("lang", () => new LangEmitter(loader()), {
+      import: { name: "LangRules" },
+    });
   },
 });
--- modules/models/src/module.ts
+++ modules/models/src/module.ts
@@ -1,9 +1,15 @@
 import { defineModule } from "@adeficior/data-modifier-core";
+import { name } from "../package.json";
 import { ModelEmitter } from "./emitter/models";
 
 export default defineModule({
-  setupEmitters: ({ register }) => {
-    register("models:block", new ModelEmitter("block"));
-    register("models:item", new ModelEmitter("item"));
+  importModule: name,
+  setup: (pack) => {
+    pack.emitter("models:block", () => new ModelEmitter("block"), {
+      import: { name: "ModelRules" },
+    });
+    pack.emitter("models:item", () => new ModelEmitter("item"), {
+      import: { name: "ModelRules" },
+    });
   },
 });
