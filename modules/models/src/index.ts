9bdb61d0fc6c669be6101480a917cb6528c618d0
Niklas Widmann <nik@somethingcatchy.net>
2026-08-22 16:55:46 +0200

    delete invalid export

--- modules/models/src/index.ts
+++ modules/models/src/index.ts
@@ -1,5 +1,4 @@
 export type { BlockstateEmitter as BlockstateRules } from "./emitter/blockstates";
-export type { ModelsService } from "./emitter/combined";
 export type { ModelEmitter as ModelRules } from "./emitter/models";
 export { default } from "./module";
 export type * from "./schema";
